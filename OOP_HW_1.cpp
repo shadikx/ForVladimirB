1e33c56f0434dda47f514456a38f99922d2b4062
shadikx <shadikx@yandex.ru>
2021-07-26 20:54:06 +0500

    1st HW

---
+++ OOP_HW_1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+class Power
+{
+private:
+	int p_first;
+	int p_second;
+public:
+	void setPower(int first, int second)
+	{
+		p_first = first;
+		p_second = second;
+	}
+
+	void calculatePower(int first, int second)
+	{
+		std::cout << pow(p_first, p_second) << std::endl;
+	}
+
+
+};
+
+int main()
+{
+	Power value;
+	value.setPower(3, 5);
+	value.calculatePower(3, 5);
+
+	return 0;
+}
---
+++ OOP_HW_1_v2.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <cstdint>
+
+class RGBA
+{
+private:
+    std::uint8_t m_red, m_green, m_blue, m_alpha;
+public:
+
+    void setRGBA(int red, int green, int blue, int alpha)
+    {
+        m_red = red;
+        m_green = green;
+        m_blue = blue;
+        m_alpha = alpha;
+    }
+
+    RGBA()
+    {
+        m_red = 0;
+        m_green = 0;
+        m_blue = 0;
+        m_alpha = 255;
+    }
+
+    RGBA(int red, int green, int blue, int alpha)
+        : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+    {
+    }
+
+    void print(int red, int green, int blue, int alpha)
+    {
+        std::cout << red << " " << green << " " << blue << " " << alpha;
+    }
+
+};
+
+int main()
+{
+    RGBA value;
+    value.setRGBA(0, 0, 0, 255);
+
+    value.print(0, 0, 0, 255);
+}
